774ff22063babb236c4ddcb044fb4712ad38aaed
Graham Hadgraft <graham.hadgraft@gmail.com>
2015-07-19 20:42:59 +0100

    Filter out passwords from the log files

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,8 @@ Bundler.require(*Rails.groups)
 
 module WranglerTodo
   class Application < Rails::Application
+    config.filter_parameters += [:password, :password_confirmation]
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
